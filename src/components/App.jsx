216e9f2b16087a0d06e8dadcd20555c93decaff3
Metaldetect <stas.klymenko@icloud.com>
2023-06-22 12:21:16 +0200

    Create game

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,93 @@
-export const App = () => {
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+const App = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const handleClick = index => {
+    if (calculateWinner(board) || board[index]) {
+      return;
+    }
+
+    const newBoard = [...board];
+    newBoard[index] = xIsNext ? 'X' : 'O';
+
+    setBoard(newBoard);
+    setXIsNext(!xIsNext);
+  };
+
+  const handleReset = () => {
+    setBoard(Array(9).fill(null));
+    setXIsNext(true);
+  };
+
+  const renderSquare = index => {
+    return (
+      <button
+        className="btn btn-light square"
+        onClick={() => handleClick(index)}
+      >
+        {board[index]}
+      </button>
+    );
+  };
+
+  const winner = calculateWinner(board);
+  const status = winner
+    ? `Winner: ${winner}`
+    : `Next Player: ${xIsNext ? 'X' : 'O'}`;
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="container">
+      <div className="board">
+        <div className="status">{status}</div>
+        <div className="row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+        <div className="reset">
+          <button className="btn btn-primary" onClick={handleReset}>
+            Reset
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
+
+const calculateWinner = board => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return board[a];
+    }
+  }
+
+  return null;
+};
+
+export default App;
